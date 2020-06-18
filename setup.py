4079a495d5fa4c6e58609e522f26b37880b85dec
Caleb Smith <macadamiadaze@gmail.com>
2020-06-18 14:03:14 -0500

    Version bump for release

--- setup.py
+++ setup.py
@@ -4,38 +4,34 @@
 from setuptools import setup
 from wildbits.__version__ import VERSION
 
-with open('docs/README.md', 'r') as readme:
+with open("docs/README.md", "r") as readme:
     long_description = readme.read()
 
 setup(
-    name='wildbits',
+    name="wildbits",
     version=VERSION,
-    author='NiceneNerd',
-    author_email='macadamiadaze@gmail.com',
-    description='A GUI frontend for Leoetlino\'s Python tools for Breath of the Wild modding',
-    long_description = long_description,
-    long_description_content_type = 'text/markdown',
-    url = 'https://github.com/NiceneNerd/Wild-Bits/',
-    include_package_data = True,
-    packages = ['wildbits'],
-    entry_points = {
-        'gui_scripts': [
-            'wildbits = wildbits.__main__:main'
-        ]
-    },
-    classifiers = [
-        'Development Status :: 4 - Beta',
-        'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
-        'Programming Language :: Python :: 3.7'
+    author="NiceneNerd",
+    author_email="macadamiadaze@gmail.com",
+    description="A GUI frontend for Leoetlino's Python tools for Breath of the Wild modding",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/NiceneNerd/Wild-Bits/",
+    include_package_data=True,
+    packages=["wildbits"],
+    entry_points={"gui_scripts": ["wildbits = wildbits.__main__:main"]},
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
+        "Programming Language :: Python :: 3.7",
     ],
-    python_requires = '>=3.7',
-    install_requires = [
-        'botw-utils >= 0.2.0',
-        'oead >= 1.1.1',
-        'rstb',
-        'pymsyt >= 0.1.5',
-        'xxhash',
-        'pywebview >= 3.2, < 4.0',
+    python_requires=">=3.7",
+    install_requires=[
+        "botw-utils >= 0.2.0",
+        "oead >= 1.1.1",
+        "rstb >= 1.2.0",
+        "pymsyt >= 0.1.5",
+        "xxhash",
+        "pywebview >= 3.2, < 4.0",
     ],
-    extras_require={"CEF": ["cefpython3~=66.0"]}
+    extras_require={"CEF": ["cefpython3~=66.0"]},
 )
--- wildbits/__version__.py
+++ wildbits/__version__.py
@@ -1,4 +1,4 @@
-VERSION = "1.0.0rc2"
+VERSION = "1.0.0"
 USER_VERSION = (
     VERSION.replace("a", " alpha ")
     .replace("rc", " release candidate ")
